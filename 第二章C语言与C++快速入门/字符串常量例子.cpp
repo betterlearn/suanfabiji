02a3f3ff006b4837fde27186ce23784018f75eca
guozekun <woxinguangmingguo@gmail.com>
2022-05-02 16:55:17 +0800

    try another git command to upload file

---
+++ 第二章C语言与C++快速入门/字符串常量例子.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    char str1[100] = "the person i like does not like me";
+    char str2[25] = "so sad a story it is";
+    printf("%s\n%s", str1, str2);
+
+    return 0;
+}
