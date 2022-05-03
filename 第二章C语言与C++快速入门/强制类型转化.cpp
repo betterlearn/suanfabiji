921bc3f225b73b3f1c4e7ba765cb0656bbafea5f
guozekun <woxinguangmingguo@gmail.com>
2022-05-03 16:11:40 +0800

    Add cast type example

---
+++ 第二章C语言与C++快速入门/强制类型转化.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    double e1 = 2.71828;
+    int a1 = 5, a2 = 2;
+    printf("%d\n", (int)e1);
+    printf("%d\n", a1 / a2);
+    printf("%0.3f", double(a1) / a2);
+
+
+    return 0;
+}
